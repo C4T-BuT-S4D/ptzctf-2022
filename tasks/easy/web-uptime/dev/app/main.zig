02f5b4ec91c09488da6bb6195a54c37b4271c679
renbou <artemmikheev1311@gmail.com>
2022-11-03 03:49:45 +0700

    uptime: configure prometheus and app crashing

--- tasks/easy/web-uptime/dev/app/main.zig
+++ tasks/easy/web-uptime/dev/app/main.zig
@@ -10,6 +10,20 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var server = try web.Server.init(gpa.allocator(), "0.0.0.0:80");
 
+    // Sleep to allow prometheus to try collecting our metrics.
+    std.time.sleep(30 * std.time.ns_per_s);
+
     _ = async server.run();
+    _ = async uptimeTest();
+
     try graceful.run();
+}
+
+// uptimeTest randomly shuts down our app to test the uptime functionalit
+fn uptimeTest() void {
+    var rnd = std.rand.DefaultPrng.init(@bitCast(u64, std.time.milliTimestamp()));
+    const timeout = 30 + rnd.random().uintAtMost(u64, 30);
+    std.log.err("crashing in {}s", .{timeout});
+    std.time.sleep(timeout * std.time.ns_per_s);
+    std.os.exit(42);
 }
