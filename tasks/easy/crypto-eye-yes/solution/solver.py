9a0d8db4d44adfc08d6d343715935074f2ff42b9
Vladimir <keltecc@ya.ru>
2022-11-22 09:00:55 +0300

    add easy-crypto-eye-yes task

---
+++ tasks/easy/crypto-eye-yes/solution/solver.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import sys
+import socket
+
+
+HOST = sys.argv[1] if len(sys.argv) > 1 else 'localhost'
+PORT = 17171
+
+
+def encrypt(io, data):
+    io.write(
+        b'ENCRYPT ' + data.hex().encode() + b'\n',
+    )
+    io.flush()
+
+    io.read(2)
+
+    return bytes.fromhex(
+        io.readline().strip().decode(),
+    )
+
+
+def decrypt(io, data):
+    io.write(
+        b'DECRYPT ' + data.hex().encode() + b'\n',
+    )
+    io.flush()
+
+    io.read(2)
+
+    return bytes.fromhex(
+        io.readline().strip().decode(),
+    )
+
+
+def xor(a, b):
+    return bytes(x ^ y for x, y in zip(a, b))
+
+
+def attack(io):
+    ct = encrypt(io, b'\x00' * 16)
+    pt = decrypt(io, ct[16:])
+
+    flag = xor(ct[:16], pt)
+    flag = b'ptzctf{' + flag + b'}'
+
+    return flag
+
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(10)
+    sock.connect((HOST, PORT))
+
+    io = sock.makefile('rwb')
+
+    try:
+        io.readline()
+        io.readline()
+
+        flag = attack(io)
+        print(flag)
+    except Exception as e:
+        print(e)
+    finally:
+        io.close()
+        sock.close()
+
+
+if __name__ == '__main__':
+    main()
